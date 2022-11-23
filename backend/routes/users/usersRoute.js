c146828de240991eed54ded11bc3008e813ee1cc
Adem Hatay <hatayadem5@gmail.com>
2022-11-24 00:47:08 +0300

    UserProfile Routes

--- backend/routes/users/usersRoute.js
+++ backend/routes/users/usersRoute.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const  {register,login, getUsers, getUser, deleteUser, updateUser} = require('../../controllers/users/usersController');
+const  {register,login, getUsers, getUser, deleteUser, updateUser, userProfile} = require('../../controllers/users/usersController');
 const authMiddleware = require('../../middlewares/auth/authMiddleware');
 
 // Fetch all users
 router.get('/',authMiddleware, getUsers);
 // Get user by id
 router.get('/:id', getUser).delete('/:id', deleteUser).put('/:id', updateUser);
+// Get Profile
+router.get('/profile/:id',authMiddleware, userProfile);
 // Register
 router.post('/register', register);
 // Login
