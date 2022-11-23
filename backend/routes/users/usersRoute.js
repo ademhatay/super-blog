9b2eb353bed18abc5d5b91fa9183a2b149a79dad
Adem Hatay <hatayadem5@gmail.com>
2022-11-24 00:56:11 +0300

    seperated userRouter for by id

--- backend/routes/users/usersRoute.js
+++ backend/routes/users/usersRoute.js
@@ -6,7 +6,11 @@ const authMiddleware = require('../../middlewares/auth/authMiddleware');
 // Fetch all users
 router.get('/',authMiddleware, getUsers);
 // Get user by id
-router.get('/:id', getUser).delete('/:id', deleteUser).put('/:id', updateUser);
+router.get('/:id', getUser);
+// Update user by id
+router.put('/:id', updateUser);
+// Delete user by id
+router.delete('/:id', deleteUser)
 // Get Profile
 router.get('/profile/:id',authMiddleware, userProfile);
 // Register
