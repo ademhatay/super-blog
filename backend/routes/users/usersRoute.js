0a8c7553703446b4e4ef8f1e01700ac815a43dd9
Adem Hatay <hatayadem5@gmail.com>
2022-11-24 01:49:22 +0300

    add auth middleware in userUpdate routing & beautify

--- backend/routes/users/usersRoute.js
+++ backend/routes/users/usersRoute.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const  {register,login, getUsers, getUser, deleteUser, updateUser, userProfile} = require('../../controllers/users/usersController');
+const { register, login, getUsers, getUser, deleteUser, updateUser, userProfile } = require('../../controllers/users/usersController');
 const authMiddleware = require('../../middlewares/auth/authMiddleware');
 
 // Fetch all users
-router.get('/',authMiddleware, getUsers);
+router.get('/', authMiddleware, getUsers);
 // Get user by id
 router.get('/:id', getUser);
 // Update user by id
-router.put('/:id', updateUser);
+router.put('/:id', authMiddleware, updateUser);
 // Delete user by id
 router.delete('/:id', deleteUser)
 // Get Profile
-router.get('/profile/:id',authMiddleware, userProfile);
+router.get('/profile/:id', authMiddleware, userProfile);
 // Register
 router.post('/register', register);
 // Login
